da3018cff80d2e9d5f625af40114b0145510e5a9
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2023-10-21 22:03:10 -0400

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section';
+import { Statistics } from './Statistics';
+import { FeedbackOptions } from './FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleAddMark = e => {
+    const option = e.target.name;
+    // this.setState({good: this.state.good + 1})
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    if (total === 0) {
+      return 0;
+    }
+    return (this.state.good / total) * 100;
+  }
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback!">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleAddMark}
+          ></FeedbackOptions>
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+export const FeedbackOptions = ({options,
+            onLeaveFeedback,}) => {
+    return (
+      <ul>
+      <li>
+        <button type="button" name="good" onClick={() => onLeaveFeedback()}>
+          Good
+        </button>
+      </li>
+      <li>
+        <button type="button" name="neutral" onClick={() => onLeaveFeedback()}>
+          Neutral
+        </button>
+      </li>
+      <li>
+        <button type="button" name="bad" onClick={() => onLeaveFeedback()}>
+          Bad
+        </button>
+      </li>
+    </ul>
+    )
+}
